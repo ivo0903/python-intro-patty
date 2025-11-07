7b4368ff43d95bf4dbf986c56a63c7de9b56e3fc
Pifd318 <ivofuica@gmail.com>
2025-11-06 23:50:58 -0300

    Elimino repositorio interno y agrego archivos como parte de mi rama

---
+++ quiz7.py
@@ -0,0 +1,4 @@
+#Pirámide numérica condicional
+#Pide al usuario un número n. Imprime una pirámide de números:
+#	Cada línea i contiene números del 1 hasta i.
+#	Si el número es múltiplo de 3, imprime "X" en lugar del número.
